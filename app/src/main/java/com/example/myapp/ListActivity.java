5958546bf2d2f0861c43ea468a2fa69522a0e781
Christian Dominic M. Climaco <climaco0401@gmail.com>
2023-03-11 16:38:55 +0800

    March 11, 2023

--- app/src/main/java/com/example/myapp/ListActivity.java
+++ app/src/main/java/com/example/myapp/ListActivity.java
@@ -3,6 +3,8 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner2);
+    // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+    // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
