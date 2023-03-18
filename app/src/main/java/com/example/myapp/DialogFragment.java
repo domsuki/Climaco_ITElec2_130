6bac0bcd5163229981420df197cc0ffac87553d5
Christian Dominic M. Climaco <climaco0401@gmail.com>
2023-03-18 18:33:18 +0800

    03/18/2023

---
+++ app/src/main/java/com/example/myapp/DialogFragment.java
@@ -0,0 +1,22 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.myapp.R;
+
+public class DialogFragment extends androidx.fragment.app.DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_dialog,container, false);
+
+    }
+}
